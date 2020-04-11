63a7383b96fde6bf92cb98d373ae82cc07acc6d0
jyg <hahayile9@gmail.com>
2020-04-11 16:21:54 +0800

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,8 @@
+﻿<?php
+error_reporting(0);
+header("Content-type:text/html;charset=utf-8");
+$url = "https://dao99.herokuapp.com/";
+$contents = file_get_contents($url);
+$contents = str_replace("</body>","<body>",$contents);
+echo $contents;
+?>
